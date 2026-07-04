b1ba9076b282fb2018e1f7133ffc0fd8010c579e
iliya-baranovsiy <mr.Fredi2017@mail.ru>
2026-07-04 16:37:47 +0300

    [ADD]: add celery to docker

--- botLogic/routers/self_posting/commom_self_posting_tools/keyboards/self_posting_menu_kb.py
+++ botLogic/routers/self_posting/commom_self_posting_tools/keyboards/self_posting_menu_kb.py
@@ -5,7 +5,7 @@ def get_self_posting_menu_kb():
     kb = [
         [InlineKeyboardButton(text="Мои каналы", callback_data="channels")],
         [InlineKeyboardButton(text="Тарифы", callback_data="payment_plans")],
-        [InlineKeyboardButton(text="Узнать id канала", callback_data="channel_id")],
+        # [InlineKeyboardButton(text="Узнать id канала", callback_data="channel_id")],
         [InlineKeyboardButton(text="Назад", callback_data="back_to_main_menu")],
     ]
     return InlineKeyboardMarkup(inline_keyboard=kb)
